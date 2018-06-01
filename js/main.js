5ab5ba69517a3fa5d00f77bdf27abb62629fe8ff
saurabh30 <gupta.saurabh711@gmail.com>
2018-06-01 10:36:04 +0530

    completed drop

--- js/main.js
+++ js/main.js
@@ -5,6 +5,14 @@ const empties = document.querySelectorAll('.empty');
 fill.addEventListener('dragstart',dragStart);
 fill.addEventListener('dragend',dragEnd);
 
+//Loop through empties and call drag events
+for(const empty of empties) {
+    empty.addEventListener('dragover',dragOver);
+    empty.addEventListener('dragenter',dragEnter);
+    empty.addEventListener('dragleave',dragLeave);
+    empty.addEventListener('drop',dragDrop);
+}
+
 //Drag functions
 function dragStart(){
     this.className += ' hold';
@@ -13,4 +21,23 @@ function dragStart(){
 
 function dragEnd(){
     this.className = 'fill';
+}
+
+function dragEnter(e) {
+    e.preventDefault;
+    this.className += ' hovered';
+}
+
+function dragLeave() {
+   this.className = 'empty';
+}
+
+function dragDrop() {
+    this.className = 'empty';
+    this.append(fill);
+}
+
+function dragOver(e) {
+   e.preventDefault();
+   
 }
